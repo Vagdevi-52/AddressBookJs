78e5c0818c5e7d2e57159add4b108c9203f365c4
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-08-06 21:31:33 -0700

    Program To Create Contact In Address Book

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,27 @@
-console.log("Welcome To AddressBook js");
+console.log("Welcome to The Address Book JS Program ") 
+class Contact
+{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNo;
+    email;
+
+    constructor(...params)
+    {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNo = params[6];
+        this.email = params[7];
+    }
+}
+
+let contact1 = new Contact("Dhulipala", "vagdevi", "vijag", "vijag", "Andhrapradesh",535558,987654321,"Dhulipalavagdevi@gmail.com");
+console.log(contact1)
